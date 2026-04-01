3e36add16581fad0b0f091787d9e24f65067729e
Kisna Agarwala <pr0zpect@Kisnas-MacBook-Pro.local>
2026-04-01 18:00:00 +0530

    Add default movie data array

---
+++ index.js
@@ -0,0 +1,15 @@
+const defaultMovies = [
+    {
+        Title: "Inception",
+        Year: "2010",
+        Poster: "https://img.omdbapi.com/?apikey=48aa168d&i=tt1375666"
+    },
+    {
+        Title: "Gladiator",
+        Year: "2000",
+        Poster: "https://img.omdbapi.com/?apikey=48aa168d&i=tt0172495"
+    }
+];
+
+let movies = [];
+let currentMovies = [];
